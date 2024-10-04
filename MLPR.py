4f90e895740b5c4a68246c3ad960502c3d0001f4
Chen <ssss@sss.com>
2024-10-04 13:02:03 +0100

    mmmmm

--- MLPR.py
+++ MLPR.py
@@ -4,7 +4,16 @@ import matplotlib.pyplot as plt
 amp_data = np.load('amp_data.npz')['amp_data']
 
 fig, ax = plt.subplots(2,1)
-
+'''
 ax[0].plot(amp_data)
 ax[1].hist(amp_data,bins=100)
 plt.show()
+'''
+
+'''
+R = len(amp_data) // 21
+dataset = amp_data[0:R*21]
+M = dataset.reshape(R,21)
+
+'''
+
