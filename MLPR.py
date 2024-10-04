ac5cfc1b273c7eeec3cce88c9a6a6a428dde1ad1
Lila <71637170+1i1a@users.noreply.github.com>
2024-10-04 12:59:57 +0100

    tidied plot for 1a

--- MLPR.py
+++ MLPR.py
@@ -3,8 +3,18 @@ import matplotlib.pyplot as plt
 
 amp_data = np.load('amp_data.npz')['amp_data']
 
+# QUESTION 1a
 fig, ax = plt.subplots(2,1)
 
 ax[0].plot(amp_data)
+ax[0].set_title('Line Graph of Sequence in amp_data.npz')
+ax[0].set_xlabel('Time Index')
+ax[0].set_ylabel('Amplitude')
+
 ax[1].hist(amp_data,bins=100)
+ax[1].set_title('Histogram of Amplitudes in amp_data.npz')
+ax[1].set_xlabel('Amplitude')
+ax[1].set_ylabel('Frequency')
+
+plt.tight_layout()
 plt.show()
